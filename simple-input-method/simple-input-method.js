99b0d25fa1284bfd8de00803a210e74c4ef84545
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-04-09 22:45:38 -0700

    中文化属性

--- simple-input-method/simple-input-method.js
+++ simple-input-method/simple-input-method.js
@@ -4,12 +4,12 @@
  */
 var SimpleInputMethod = 
 {
-	hanzi: '', // 候选汉字
-	pinyin: '', // 候选拼音
-	result: [], // 当前匹配到的汉字集合
-	pageCurrent: 1, // 当前页
-	pageSize: 5, // 每页大小
-	pageCount: 0, // 总页数
+	候选汉字: '', // 候选汉字
+	候选拼音: '', // 候选拼音
+	匹配汉字: [], // 当前匹配到的汉字集合
+	当前页: 1, // 当前页
+	每页大小: 5, // 每页大小
+	总页数: 0, // 总页数
 	/**
 	 * 初始化字典配置
 	 */
@@ -55,14 +55,14 @@ var SimpleInputMethod =
 			if(target.nodeName == 'LI') that.按索引选字(parseInt(target.dataset.idx));
 			else if(target.nodeName == 'SPAN')
 			{
-				if(target.className == 'page-up' && that.pageCurrent > 1)
+				if(target.className == 'page-up' && that.当前页 > 1)
 				{
-					that.pageCurrent--;
+					that.当前页--;
 					that.刷新当前页();
 				}
-				else if(target.className == 'page-down' && that.pageCurrent < that.pageCount)
+				else if(target.className == 'page-down' && that.当前页 < that.总页数)
 				{
-					that.pageCurrent++;
+					that.当前页++;
 					that.刷新当前页();
 				}
 			}
@@ -92,30 +92,30 @@ var SimpleInputMethod =
 					that.加英文字符(String.fromCharCode(keyCode+32), this);
 					preventDefault = true;
 				}
-				else if(keyCode == 8 && that.pinyin) // 删除键
+				else if(keyCode == 8 && that.候选拼音) // 删除键
 				{
 					that.删英文字符();
 					preventDefault = true;
 				}
-				else if(keyCode >= 48 && keyCode <= 57 && !e.shiftKey && that.pinyin) // 1-9
+				else if(keyCode >= 48 && keyCode <= 57 && !e.shiftKey && that.候选拼音) // 1-9
 				{
 					that.按索引选字(keyCode-48);
 					preventDefault = true;
 				}
-				else if(keyCode == 32 && that.pinyin) // 空格
+				else if(keyCode == 32 && that.候选拼音) // 空格
 				{
 					that.按索引选字(1);
 					preventDefault = true;
 				}
-				else if(keyCode == 33 && that.pageCount > 0 && that.pageCurrent > 1) // 上翻页
+				else if(keyCode == 33 && that.总页数 > 0 && that.当前页 > 1) // 上翻页
 				{
-					that.pageCurrent--;
+					that.当前页--;
 					that.刷新当前页();
 					preventDefault = true;
 				}
-				else if(keyCode == 34 && that.pageCount > 0 && that.pageCurrent < that.pageCount) // 下翻页
+				else if(keyCode == 34 && that.总页数 > 0 && that.当前页 < that.总页数) // 下翻页
 				{
-					that.pageCurrent++;
+					that.当前页++;
 					that.刷新当前页();
 					preventDefault = true;
 				}
@@ -172,18 +172,18 @@ var SimpleInputMethod =
 	 */
 	按索引选字: function(i)
 	{
-		var hz = this.result[(this.pageCurrent - 1) * this.pageSize + i - 1];
+		var hz = this.匹配汉字[(this.当前页 - 1) * this.每页大小 + i - 1];
 		if(!hz) return;
-		this.hanzi += hz;
-		var idx = this.pinyin.indexOf("'");
+		this.候选汉字 += hz;
+		var idx = this.候选拼音.indexOf("'");
 		if(idx > 0)
 		{
-			this.pinyin = this.pinyin.substr(idx+1);
+			this.候选拼音 = this.候选拼音.substr(idx+1);
 			this.刷新();
 		}
 		else // 如果没有单引号，表示已经没有候选词了
 		{
-			this._input.value += this.hanzi;
+			this._input.value += this.候选汉字;
 			this.隐藏();
 		}
 	},
@@ -192,45 +192,45 @@ var SimpleInputMethod =
 	 */
 	刷新: function()
 	{
-		var temp = this.拼音转汉字(this.pinyin.replace(/'/g, ''));
-		this.result = temp[0];
-		this.pinyin = temp[1];
-		var count = this.result.length;
-		this.pageCurrent = 1;
-		this.pageCount = Math.ceil(count / this.pageSize);
-		this._pinyinTarget.innerHTML = this.hanzi + this.pinyin;
+		var temp = this.拼音转汉字(this.候选拼音.replace(/'/g, ''));
+		this.匹配汉字 = temp[0];
+		this.候选拼音 = temp[1];
+		var count = this.匹配汉字.length;
+		this.当前页 = 1;
+		this.总页数 = Math.ceil(count / this.每页大小);
+		this._pinyinTarget.innerHTML = this.候选汉字 + this.候选拼音;
 		this.刷新当前页();
 	},
 	刷新当前页: function()
 	{
-		var temp = this.result.slice((this.pageCurrent-1)*this.pageSize, this.pageCurrent*this.pageSize);
+		var temp = this.匹配汉字.slice((this.当前页-1)*this.每页大小, this.当前页*this.每页大小);
 		var html = '';
 		var i = 0;
 		temp.forEach(function(val)
 		{
 			html += '<li data-idx="'+(++i)+'">' + val + '</li>';
 		});
-		this._target.querySelector('.page-up').style.opacity = this.pageCurrent > 1 ? '1' : '.3';
-		this._target.querySelector('.page-down').style.opacity = this.pageCurrent < this.pageCount ? '1' : '.3';
+		this._target.querySelector('.page-up').style.opacity = this.当前页 > 1 ? '1' : '.3';
+		this._target.querySelector('.page-down').style.opacity = this.当前页 < this.总页数 ? '1' : '.3';
 		this._resultTarget.innerHTML = html;
 	},
 	加英文字符: function(ch, obj)
 	{
-		if(this.pinyin.length == 0) // 长度为1，显示输入法
+		if(this.候选拼音.length == 0) // 长度为1，显示输入法
 		{
 			this.显示(obj);
 		}
-		this.pinyin += ch;
+		this.候选拼音 += ch;
 		this.刷新();
 	},
 	删英文字符: function()
 	{
-		if(this.pinyin.length <= 1)
+		if(this.候选拼音.length <= 1)
 		{
 			this.隐藏();
 			return;
 		}
-		this.pinyin = this.pinyin.substr(0, this.pinyin.length-1);
+		this.候选拼音 = this.候选拼音.substr(0, this.候选拼音.length-1);
 		this.刷新();
 	},
 	显示: function(obj)
@@ -248,11 +248,11 @@ var SimpleInputMethod =
 	},
 	重置: function()
 	{
-		this.hanzi = '';
-		this.pinyin = '';
-		this.result = [];
-		this.pageCurrent = 1;
-		this.pageCount = 0;
+		this.候选汉字 = '';
+		this.候选拼音 = '';
+		this.匹配汉字 = [];
+		this.当前页 = 1;
+		this.总页数 = 0;
 		this._pinyinTarget.innerHTML = '';
 	}
 };
